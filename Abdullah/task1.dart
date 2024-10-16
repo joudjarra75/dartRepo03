ccaf6c511a1d4a3e612d4d80ad765a3b9f84e2da
Joud Al-jarrah <72281607+joudjarra75@users.noreply.github.com>
2024-10-16 17:55:22 +0300

    Create task1.dart

---
+++ Abdullah/task1.dart
@@ -0,0 +1,19 @@
+import 'package:intro/intro.dart' as intro;
+
+main() {
+  String name = "ahmad";
+  int age = 25;
+  String $name = "ll";
+  print("hello {$name} your age :${age + 5}");
+// hello ahmad your age : 25
+
+  String multiLines = """hello world
+ddsfdsfs
+hdfh
+ghrghdr
+ """;
+  print(multiLines);
+}
+///
+//
+//
